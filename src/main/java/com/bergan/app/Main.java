9e22a7e359607afc64dc623aa9076eea7316cf9d
Eric Bergan <bootcamp.ejb@gmail.com>
2021-12-18 15:04:57 -0600

    adding project

---
+++ src/main/java/com/bergan/app/Main.java
@@ -0,0 +1,95 @@
+package com.bergan.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    static String[] gridNums = new String[]{null, "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    static int[] xArray = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    static int[] oArray = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    static boolean flipper = true;
+
+    public static void createGrid() {
+
+
+        String[] row1 = {" " + gridNums[1] + " ", "|", " " + gridNums[2] + " ", "|", " " + gridNums[3] + " "};
+        String[] row2 = {" " + gridNums[4] + " ", "|", " " + gridNums[5] + " ", "|", " " + gridNums[6] + " "};
+        String[] row3 = {" " + gridNums[7] + " ", "|", " " + gridNums[8] + " ", "|", " " + gridNums[9] + " "};
+
+        System.out.println(String.join("", row1));
+        System.out.println("---|---|---");
+        System.out.println(String.join("", row2));
+        System.out.println("---|---|---");
+        System.out.println(String.join("", row3));
+
+        getInput();
+    }
+
+    public static void getInput() {
+        int square = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number 1-9 corresponding to an unclaimed square.");
+
+        do {
+            try {
+                String input = sc.nextLine();
+                square = Integer.parseInt(input);
+
+                if (square == 0) return;
+
+                if (square < 1 || square > 9) {
+                    throw new IllegalArgumentException("Number 1-9 only please or 0 to exit");
+                }
+
+                if (gridNums[square].equals(ANSI_YELLOW + "X" + ANSI_RESET) ||
+                        gridNums[square].equals(ANSI_RED + "O" + ANSI_RESET)) {
+                    System.out.println(gridNums[square]);
+                    System.out.println("\nThat space is taken!");
+                    square = 0;
+                }
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("Number 1-9 only please or 0 to exit");
+                createGrid();
+            }
+            catch (Exception e) {
+                System.out.print("\nNumber 1-9 only please or 0 to exit");
+            }
+        } while (square == 0);
+
+        updateGrid(square);
+    }
+
+    public static void updateGrid(int square) {
+        System.out.println("square: " + square);
+        gridNums[square] = flipper ? ANSI_YELLOW + "X" + ANSI_RESET : ANSI_RED + "O" + ANSI_RESET;
+
+        UsefulMethods.updateWinningArrays(square);
+
+        if (Arrays.stream(xArray).anyMatch(i -> i == 3)) {
+            System.out.println("\nX Wins!!!");
+            return;
+        }
+
+        if (Arrays.stream(oArray).anyMatch(i -> i == 3)) {
+            System.out.println("\nX Wins!!!");
+            return;
+        }
+
+        flipper = !flipper;
+
+        createGrid();
+    }
+
+    public static void main(String[] args) {
+        createGrid();
+    }
+}
---
+++ src/main/java/com/bergan/app/UsefulMethods.java
@@ -0,0 +1,105 @@
+package com.bergan.app;
+
+public class UsefulMethods {
+
+    public static void updateWinningArrays(int square) {
+        switch (square) {
+            case 1:
+                if (Main.flipper) {
+                    Main.xArray[1]++;
+                    Main.xArray[4]++;
+                    Main.xArray[7]++;
+                } else {
+                    Main.oArray[1]++;
+                    Main.oArray[4]++;
+                    Main.oArray[7]++;
+                }
+                break;
+            case 2:
+                if (Main.flipper) {
+                    Main.xArray[1]++;
+                    Main.xArray[5]++;
+                } else {
+                    Main.oArray[1]++;
+                    Main.oArray[5]++;
+                }
+                break;
+            case 3:
+                if (Main.flipper) {
+                    Main.xArray[1]++;
+                    Main.xArray[6]++;
+                    Main.xArray[8]++;
+                } else {
+                    Main.oArray[1]++;
+                    Main.oArray[6]++;
+                    Main.oArray[8]++;
+                }
+                break;
+            case 4:
+                if (Main.flipper) {
+                    Main.xArray[2]++;
+                    Main.xArray[4]++;
+                } else {
+                    Main.oArray[2]++;
+                    Main.oArray[4]++;
+                }
+                break;
+            case 5:
+                if (Main.flipper) {
+                    Main.xArray[2]++;
+                    Main.xArray[5]++;
+                    Main.xArray[7]++;
+                    Main.xArray[8]++;
+                } else {
+                    Main.oArray[2]++;
+                    Main.oArray[5]++;
+                    Main.xArray[7]++;
+                    Main.oArray[8]++;
+                }
+                break;
+            case 6:
+                if (Main.flipper) {
+                    Main.xArray[2]++;
+                    Main.xArray[6]++;
+                } else {
+                    Main.oArray[2]++;
+                    Main.oArray[6]++;
+                }
+                break;
+            case 7:
+                if (Main.flipper) {
+                    Main.xArray[3]++;
+                    Main.xArray[7]++;
+                    Main.xArray[8]++;
+                } else {
+                    Main.oArray[3]++;
+                    Main.oArray[7]++;
+                    Main.oArray[8]++;
+                }
+                break;
+            case 8:
+                if (Main.flipper) {
+                    Main.xArray[3]++;
+                    Main.xArray[5]++;
+                } else {
+                    Main.oArray[3]++;
+                    Main.oArray[5]++;
+                }
+                break;
+            case 9:
+                if (Main.flipper) {
+                    Main.xArray[3]++;
+                    Main.xArray[6]++;
+                    Main.xArray[7]++;
+                } else {
+                    Main.oArray[3]++;
+                    Main.oArray[6]++;
+                    Main.oArray[7]++;
+                }
+                break;
+            default:
+                // todo not sure what to do here
+        }
+    }
+
+}
